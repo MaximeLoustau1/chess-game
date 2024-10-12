97ed6b18db0c1493590e51f48c3f89d8f0c08802
MaximeLoustau1 <122365264+MaximeLoustau1@users.noreply.github.com>
2024-10-12 20:39:15 +0200

    cleaned up main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 #TODO:
-#   - implement view_last_board_state functionality ?!
+#   - implement view_last_board_state functionality 
 #   - option to choose what piece to promote pawn to
-#   - start and end game loading screen
 #   - show possible moves for castling should display the squares where the king and rook will be
-#   - when in check, it doesnt let other pieces block the path, it only lets other piece move to take the attacking piece
-#   - if king can move anywhere but checked squares, make other pieces move anywhere but not checked squares
-#   - prevent pinned pieces from moving
-
-##next step would be to handle double checks, pinned pieces and stalemates
-##stalemate::>>make a function that checks if white/black has any possible moves that won't be king_walks_in_check() and if None then stalemate
 
 import pygame
 from gameLogic import Game
